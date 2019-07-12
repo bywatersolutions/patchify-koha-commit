a411bf3421dfd6736933a0dad8c32c40e9f67dea
Kyle M Hall <kyle@bywatersolutions.com>
2019-07-12 11:05:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# patchify-koha-commit
+Convert a git-derived Koha patch into a patch that can be applied to a server running a Debian package install of Koha.
+
+## Usage
+
+```bash
+patchify.pl -i|--input input.patch -o|--output output.patch -v|--verbose
+```
---
+++ patchify.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use feature qw(say);
+
+use Modern::Perl;
+use File::Slurp;
+use Getopt::Long;
+
+my $input_file;
+my $output_file;
+my $verbose;
+
+GetOptions(
+    "i|input=s"  => \$input_file,
+    "o|output=s" => \$output_file,
+    "verbose"    => \$verbose,
+) or die("Error in command line arguments\n");
+
+unless ($input_file) {
+    say "-i --input <path to patch file> is required";
+    exit 1;
+}
+unless ($output_file) {
+    say "-o --output <path to patch file> is required";
+    exit 1;
+}
+
+my @lines = read_file($input_file);
+
+my $mapping = {
+    'b/C4/' => '/usr/share/koha/lib/C4/',
+    'b/t/'  => '/tmp/',
+};
+
+my $b_line = qr/^\+\+\+ b\//;
+
+foreach my $line (@lines) {
+    if ( $line =~ m/$b_line/ ) {    # match on
+        print "FOUND LINE: $line" if $verbose;
+
+        foreach my $from ( keys %$mapping ) {
+            my $to = $mapping->{$from};
+            $line =~ s/$from/$to/;
+        }
+
+        print "NO MAPPING FOUND FOR $line" if $line =~ m/$b_line/;
+    }
+}
+
+write_file( $output_file, @lines );
